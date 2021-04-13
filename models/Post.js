8b8275f0939434df01ce6728e5495ff7d80c1508
Andrew Lee <andrewleee10@gmail.com>
2021-04-13 13:26:37 -0700

    index boiler plate and post model

---
+++ models/Post.js
@@ -0,0 +1,17 @@
+const { model, Schema } = require('mongoose')
+
+const Post = new Schema({
+  post_title: String,
+  post_content: String, 
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  likes: Number,
+  comments: {
+    type: Schema.Types.ObjectId,
+    ref: 'Comment'
+  }
+})
+
+module.exports = model('Post', Post)
