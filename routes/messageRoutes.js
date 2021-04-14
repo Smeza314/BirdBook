8046f3fe251ddd98b29d9fdf8677c9d50110c65e
justinjarrard <jjarrard21@gmail.com>
2021-04-14 14:12:43 -0400

    messageroutes

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,27 +1,27 @@
 const router = require('express').Router()
 const { Message } = require('../models')
 
-router.get('/messages', (req, res) => {
+router.get('/messages', passport.authenticate('jwt'), (req, res) => {
   Message.find({})
     .then(message => res.json(message))
     .catch(err => console.log(err))
 })
 
-router.post('/messages', (req, res) => {
+router.post('/messages', passport.authenticate('jwt'), (req, res) => {
   Message.create(req.body)
     .then(message => res.json(message))
     .catch(err => console.log(err))
 })
 
-router.delete('/messages/:id', (req, res) => {
+router.delete('/messages/:id', passport.authenticate('jwt'), (req, res) => {
   Message.findByIdAndDelete(req.params.id)
     .then(() => res.sendStatus(200))
     .catch(err => console.log(err))
 })
 
-router.put('/messages/:id'), (req, res) => {
+router.put('/messages/:id', passport.authenticate('jwt'), (req, res) => {
   Message.findByIdAndUpdate(req.params.id, req.body)
-  .then(( => res.SendStatus(200))
+  .then(() => res.SendStatus(200))
   .catch(err=> console.log(err))
 })
 
