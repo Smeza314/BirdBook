7e41339c55cfc43295c77f7bd2c4f30deb7d1901
Andrew Lee <andrewleee10@gmail.com>
2021-04-19 10:20:39 -0700

    deleted unused packages

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -2,15 +2,6 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import { borders } from '@material-ui/system';
 import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles((theme) => ({
