dd29af6faa9f5f762c22dc3ba3db202c5d5e01ea
justinjarrard <jjarrard21@gmail.com>
2021-04-20 17:22:04 -0400

    errors

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -90,7 +90,7 @@ const Profile = () => {
             <Divider />
           </Grid>
           {
-            userState.user.posts > 0
+            userState.user.posts.length > 0
               ? userState.user.posts.slice(0).reverse().map(post => (
                 <Post
                   username={post.author.username}
